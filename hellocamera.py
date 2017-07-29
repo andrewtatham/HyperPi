1bfdad060bb2f8332c6aa61c29f4664f580c49bc
Andrew Tatham <andrewtatham@gmail.com>
2017-07-29 11:10:39 +0100

    pygame camera

--- hellocamera.py
+++ hellocamera.py
@@ -5,7 +5,8 @@ import pygame.camera
 from pygame.locals import *
 
 DEVICE = '/dev/video0'
-SIZE = (800, 480)
+screen_size = (800, 480)
+camera_size = (1280, 720)
 FILENAME = 'capture.png'
 
 
@@ -15,17 +16,19 @@ def camstream():
 
     pprint.pprint(pygame.camera.list_cameras())
 
-    display = pygame.display.set_mode(SIZE, pygame.FULLSCREEN)
-    camera = pygame.camera.Camera(DEVICE, SIZE)
+    display = pygame.display.set_mode(screen_size, pygame.FULLSCREEN)
+    camera = pygame.camera.Camera(DEVICE, camera_size)
     camera.start()
 
     actual_camera_size = camera.get_size()
     pprint.pprint(actual_camera_size)
 
-    screen = pygame.surface.Surface(SIZE, 0, display)
+    screen = pygame.surface.Surface(screen_size, 0, display)
+    frame = pygame.surface.Surface(camera_size, 0, display)
     capture = True
     while capture:
-        screen = camera.get_image(screen)
+        frame = camera.get_image(frame)
+        screen = pygame.transform.scale(frame,screen_size, screen)
         display.blit(screen, (0, 0))
         pygame.display.flip()
         for event in pygame.event.get():
