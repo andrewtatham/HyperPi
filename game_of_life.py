bfe9aa1999ff1e72ed2756eaae3433ce5507dc8b
Andrew Tatham <andrewtatham@gmail.com>
2018-06-20 21:50:25 +0100

    game of life

--- game_of_life.py
+++ game_of_life.py
@@ -1,16 +1,28 @@
-import pprint
+import collections
+import platform
 import random
-from time import sleep
-from random import randint
+
 import pygame
 from pygame.rect import Rect
 
 pygame.init()
-
+clock = pygame.time.Clock()
+fps = 10
 # The screen size in pixels
 screen_x = 800
 screen_y = 600  # Height of screen in pixels
-screen = pygame.display.set_mode((screen_x, screen_y), 0, 24)  # New 24-bit screen
+
+_platform = platform.platform()
+print(_platform)
+is_linux = _platform.startswith('Linux')
+_node = platform.node()
+print(_node)
+is_full_screen = is_linux
+
+display_flags = 0
+if is_full_screen:
+    display_flags += pygame.FULLSCREEN
+screen = pygame.display.set_mode((screen_x, screen_y), display_flags)
 
 # The size of each cell in pixels
 cell_x = 16
@@ -25,10 +37,15 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 
 
 def one_in(n):
     return random.randint(0, n) == 0
+
+
+def all_same(items):
+    return all(x == items[0] for x in items)
 
 
 class Cell(object):
@@ -37,41 +54,56 @@ class Cell(object):
         self.y = y
         self.x_pixels = x * cell_x
         self.y_pixels = y * cell_y
-        self.top_left = [self.x_pixels, self.y_pixels]
-        self.bottom_right = [self.x_pixels + cell_x, self.y_pixels + cell_y]
-        self.rect = Rect(self.top_left, self.bottom_right)
+
+        self.rect = Rect(self.x_pixels, self.y_pixels, cell_x, cell_y)
+
         self.is_alive = alive
         self.was_alive = alive
+        self.changed = False
+        self.age = 0
+
         self.neighbours = None
 
     def set_neighbours(self, neighbours):
         self.neighbours = neighbours
 
     def before_evolve(self):
+        self.changed = False
         self.was_alive = self.is_alive
 
     def evolve(self):
         alive_neighbour_count = sum(map(lambda n: n.was_alive, self.neighbours))
         self.is_alive = (self.was_alive and 2 <= alive_neighbour_count <= 3) \
                         or (not self.was_alive and alive_neighbour_count == 3)
+        self.changed = self.is_alive != self.was_alive
+        if self.changed:
+            self.age = 0
+        else:
+            self.age += 1
+        return self.changed
 
-
-    def render(self, force=False):
-        changed = self.is_alive != self.was_alive
-        if changed or force:
+    def render(self, colour=None):
+        if not colour:
             if self.is_alive:
-                colour = GREEN
+                if self.age < 2:
+                    colour = GREEN
+                elif self.age < 4:
+                    colour = YELLOW
+                else:
+                    colour = RED
             else:
                 colour = BLACK
-            pygame.draw.rect(screen, colour, self.rect, 0)
+        pygame.draw.rect(screen, colour, self.rect, 0)
 
     def is_collision(self, pos):
-        return self.rect.collidepoint(pos)
+        collision = self.rect.collidepoint(pos)
+        return collision
 
     def on_click(self):
-        print("on_click")
+        self.toggle()
+
+    def toggle(self):
         self.is_alive = not self.is_alive
-        self.render(force=True)
 
 
 class Grid(object):
@@ -86,9 +118,6 @@ class Grid(object):
                     (x + 0, y - 1), (x + 0, y + 1),
                     (x + 1, y - 1), (x + 1, y + 0), (x + 1, y + 1)
                 ]
-                # Remove offscreen cells
-                # neighbour_cell_positions = list(filter(
-                #    lambda other: 0 <= other[0] < n_cells_x and 0 <= other[1] < n_cells_y, neighbour_cell_positions))
                 neighbours = list(
                     map(lambda other: self.cells[other[0] % n_cells_x][other[1] % n_cells_y], neighbour_cell_positions))
                 self.cells[x][y].set_neighbours(neighbours)
@@ -97,14 +126,18 @@ class Grid(object):
         for x in range(n_cells_x):
             for y in range(n_cells_y):
                 self.cells[x][y].before_evolve()
+        n_changed = 0
         for x in range(n_cells_x):
             for y in range(n_cells_y):
-                self.cells[x][y].evolve()
+                n_changed += self.cells[x][y].evolve()
+        return n_changed
 
-    def render(self, force=False):
+    def render(self):
         for x in range(n_cells_x):
             for y in range(n_cells_y):
-                self.cells[x][y].render(force)
+                self.cells[x][y].render()
+        pygame.display.flip()
+        clock.tick(fps)
 
     def handle_click(self, position):
         for x in range(n_cells_x):
@@ -113,36 +146,44 @@ class Grid(object):
                 if cell.is_collision(position):
                     cell.on_click()
 
+    def add_noise(self, n):
+        for _ in range(n):
+            cell = self.get_random_cell()
+            cell.toggle()
+
+    def get_random_cell(self):
+        x = random.randint(0, n_cells_x - 1)
+        y = random.randint(0, n_cells_y - 1)
+        cell = self.cells[x][y]
+        return cell
+
 
 class GameOfLife(object):
     def __init__(self):
 
         init_alive = [[0 for _ in range(n_cells_y)] for _ in range(n_cells_x)]
 
-        for x in range(n_cells_x):
-            for y in range(n_cells_y):
-                init_alive[x][y] = one_in(10)
+        # for x in range(n_cells_x):
+        #     for y in range(n_cells_y):
+        #         init_alive[x][y] = one_in(6)
 
         self.grid = Grid(init_alive)
-        self.clock = pygame.time.Clock()
 
-    def run(self, fps=60):
-        # force initial render for first frame
-
-        self.grid.render(force=True)
-        pygame.display.flip()
-        self.clock.tick(fps)
+    def run(self):
+        self.grid.render()
         done = False
+        activity_monitor = collections.deque(maxlen=5)
         while not done:
-            self.grid.evolve()
-            self.grid.render(force=True)
-            pygame.display.flip()
-            self.clock.tick(fps)
+            n_changed = self.grid.evolve()
+            activity_monitor.append(n_changed)
+            if all_same(activity_monitor):
+                self.grid.add_noise(100)
+
+            self.grid.render()
             for event in pygame.event.get():
-                print(event)
                 if event.type == pygame.QUIT:
                     done = True
-                elif event.type == pygame.MOUSEBUTTONUP:
+                elif event.type == pygame.MOUSEBUTTONDOWN:
                     position = pygame.mouse.get_pos()
                     self.grid.handle_click(position)
 
